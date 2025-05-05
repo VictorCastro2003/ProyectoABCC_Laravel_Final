b8aca5ec0c867c5687a8c48e8a09b4ae03db2ee8
VictorCastro2003 <125502355+VictorCastro2003@users.noreply.github.com>
2025-05-04 22:58:26 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,28 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    plugins: [laravel({
-        input: ['resources/css/app.css', 'resources/js/app.js'],
-        refresh: true,
-    })],
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+    ],
+    // Configuración crucial para producción:
+    base: process.env.NODE_ENV === 'production' ? '/build/' : '/',
+    build: {
+        manifest: true,
+        outDir: 'public/build',
+        rollupOptions: {
+            output: {
+                entryFileNames: 'assets/[name].[hash].js',
+                chunkFileNames: 'assets/[name].[hash].js',
+                assetFileNames: 'assets/[name].[hash].[ext]',
+            }
+        }
+    },
+    server: {
+        hmr: {
+            host: 'localhost',
+        },
+    }
 });
-
