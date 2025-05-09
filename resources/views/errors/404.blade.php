22eb32003e3d9acf07bbd051a29ab00626105218
VictorCastro2003 <125502355+VictorCastro2003@users.noreply.github.com>
2025-05-08 21:21:25 -0600

    Update 404.blade.php

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Página no encontrada | Nombre de tu Aplicación</title>
+    <title>Página no encontrada | Sistema de Tutorias</title>
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Font Awesome -->
