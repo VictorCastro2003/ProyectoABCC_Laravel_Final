8f353ee7d13813ff2b57274441642192224763af
VictorCastro2003 <125502355+VictorCastro2003@users.noreply.github.com>
2025-05-17 23:09:10 -0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\AlumnoController;
+use App\Http\Controllers\AlumnoMateriaController;
+use App\Http\Controllers\CalificacionController;
+
 
 /*
 |--------------------------------------------------------------------------
