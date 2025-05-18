bf255034d5071b7b9b8e4eb09905659b5c405c8e
VictorCastro2003 <125502355+VictorCastro2003@users.noreply.github.com>
2025-05-18 15:47:42 -0600

    Update AlumnoMateriaController.php

--- app/Http/Controllers/AlumnoMateriaController.php
+++ app/Http/Controllers/AlumnoMateriaController.php
@@ -13,15 +13,32 @@ class AlumnoMateriaController extends Controller
     return view('asignar_materias', compact('alumno', 'materias'));
 }
 
-public function asignar(Request $request, Alumno $alumno)
+ public function asignar(Request $request, Alumno $alumno)
+    {
+        $request->validate([
+            'materias' => 'required|array|max:6',
+            'semestre' => 'required|integer|min:1|max:12'
+        ]);
+
+        foreach ($request->materias as $materiaId) {
+            $alumno->materias()->syncWithoutDetaching([
+                $materiaId => ['semestre' => $request->semestre]
+            ]);
+        }
+
+        return redirect()->route('alumnos.show', $alumno->Num_Control)
+            ->with('success', 'Materias asignadas correctamente al semestre especificado.');
+    }
+    public function filtrarMaterias(Request $request, Alumno $alumno)
 {
-    $request->validate([
-        'materias' => 'required|array|max:6'
+    $semestreSeleccionado = $request->semestre;
+    
+    $materiasFiltradas = $alumno->materias->filter(function ($materia) use ($semestreSeleccionado) {
+        return !$semestreSeleccionado || $materia->pivot->semestre == $semestreSeleccionado;
+    });
+
+    return response()->json([
+        'html' => view('partials.tabla_materias', compact('materiasFiltradas'))->render()
     ]);
-
-    $alumno->materias()->syncWithoutDetaching($request->materias);
-
-    return redirect()->route('alumnos.show', $alumno->Num_Control)
-    ->with('success', 'Materias asignadas correctamente.');
 }
 }
