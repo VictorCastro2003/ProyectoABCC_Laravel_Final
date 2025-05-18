f3b3131d72aac3bfed87f02dd7b7cf5e98b3fbe5
VictorCastro2003 <125502355+VictorCastro2003@users.noreply.github.com>
2025-05-17 21:58:36 -0600

    Add files via upload

---
+++ app/Http/Controllers/AlumnoMateriaController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Alumno;
+use Illuminate\Http\Request;
+use App\Models\Materia;
+
+class AlumnoMateriaController extends Controller
+{
+    public function form(Alumno $alumno)
+{
+    $materias = Materia::where('carrera', $alumno->Carrera)->get();
+    return view('asignar_materias', compact('alumno', 'materias'));
+}
+
+public function asignar(Request $request, Alumno $alumno)
+{
+    $request->validate([
+        'materias' => 'required|array|max:6'
+    ]);
+
+    $alumno->materias()->syncWithoutDetaching($request->materias);
+
+    return redirect()->route('alumnos.index')->with('success', 'Materias asignadas correctamente.');
+}
+}
---
+++ app/Http/Controllers/CalificacionController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Alumno;
+
+class CalificacionController extends Controller
+{
+    public function create(Alumno $alumno)
+    {
+        // Traer materias asignadas con la calificación directamente desde la tabla pivote
+        $materiasAsignadas = $alumno->materias;
+
+        return view('calificaciones', compact('alumno', 'materiasAsignadas'));
+    }
+
+    public function store(Request $request, Alumno $alumno)
+    {
+        $request->validate([
+            'calificaciones' => 'required|array',
+            'calificaciones.*' => 'required|numeric|min:0|max:100',
+        ]);
+
+        foreach ($request->calificaciones as $materiaId => $nota) {
+            $alumno->materias()->updateExistingPivot($materiaId, [
+                'calificacion' => $nota,
+            ]);
+        }
+
+        return redirect()->route('alumnos.index')->with('success', 'Calificaciones registradas correctamente.');
+    }
+}
